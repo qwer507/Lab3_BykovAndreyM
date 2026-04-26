8f6957a7ea5e738373b1b36079633e1edadbbe8d
RedPen <bykovam@mer.ci.nsu.ru>
2026-04-26 18:56:59 +0700

    add class OtherProgramInteraction

---
+++ ConsoleApp1/Classes/OtherProgramInteraction.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    public class OtherProgramInteraction : IOtherProgramInteraction
+    {
+        public void SendRegInfoToEmail(string login, string password)
+        {
+            Thread.Sleep(1000);
+        }
+    }
+}
