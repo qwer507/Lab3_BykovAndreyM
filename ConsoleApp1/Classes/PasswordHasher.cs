077a8d7977ad796060d6ca0decf8241606772d2c
RedPen <bykovam@mer.ci.nsu.ru>
2026-04-26 17:55:38 +0700

    add class PasswordHasher

---
+++ ConsoleApp1/Classes/PasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1.Classes
+{
+    public static class PasswordHasher
+    {
+        public static string MaskPassword(string? password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return "***";
+
+            using var sha256 = SHA256.Create();
+            var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            var hashString = Convert.ToBase64String(hash);
+            return hashString;
+        }
+    }
+}
