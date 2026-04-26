237ccf849d6eaa13182b6a3cc32949966d4d22ce
RedPen <bykovam@mer.ci.nsu.ru>
2026-04-26 18:34:44 +0700

    add class UserInteraction

---
+++ ConsoleApp1/Classes/UserInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Interfaces;
+
+namespace ConsoleApp1.Classes
+{
+    public class UserInteraction : IUserInteraction
+    {
+        public List<string?> GetRegData()
+        {
+
+            Console.WriteLine("Введите логин: ");
+            string? login = Console.ReadLine();
+            Console.WriteLine("Введите пароль: ");
+            string? password = Console.ReadLine();
+            Console.WriteLine("Введите пароль еще раз: ");
+            string? passwordRepeat = Console.ReadLine();
+            
+           return new List<string?> {login, password, passwordRepeat };
+        }
+    }
+}
