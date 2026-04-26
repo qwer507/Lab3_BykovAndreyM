35cc0b2def23440bddc8fe30ddc61eff3249418c
RedPen <bykovam@mer.ci.nsu.ru>
2026-04-26 19:05:53 +0700

    .

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            // подключить бд
+            // создать класс для работы с бд
+            // создать Contoller
+            // написать тесты
             Console.WriteLine("Hello, World!");
         }
     }
